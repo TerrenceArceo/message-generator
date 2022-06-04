2e076535c510e7fbf21fa1e336d2b6ce2724c701
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2022-06-04 15:36:47 -0700

    create basic nested arrays inside an object to create random messages later on using a function

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('hello')
+const messages = {
+    message1: [
+        "You look",
+        "I think you're",
+        "you're so",
+    ],
+    message2: [
+        "beautiful",
+        "nice",
+        "kind",
+        "compassionate",
+        "wonderful",
+        "gorgeous",
+        "brave",
+        "smart",
+        "strong"
+    ],
+    message3:[
+        "I envy you",
+        "may I get your number?",
+        "I'd love you take you on a date",
+        "I appreciate it!",
+        "Keep it up!"
+    ]
+}
+
